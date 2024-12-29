98f8df9178d35e84494e1448b3e84ef159c01e6b
ajwildr <ajk519451@gmail.com>
2024-12-29 17:13:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require 'includes/db_connect.php';
+// require 'includes/db_connect.php';
 
 // Redirect to login if user is not logged in
 if (!isset($_SESSION['user_id'])) {
