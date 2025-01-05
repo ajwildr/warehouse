8e5dc2125f6830e8cc4287e406072e6895cc235d
ajwildr <ajk519451@gmail.com>
2025-01-05 14:42:15 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -6,6 +6,6 @@ session_unset();  // Removes all session variables
 session_destroy();  // Destroys the session
 
 // Redirect to the login page
-header("Location: index.php");
+header("Location: home.php");
 exit;
 ?>
